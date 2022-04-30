57cf31b826be4e7e711cd2ca46e44aa526c9b178
류 상원 <ryuuuuuuuu@ajou.ac.kr>
2022-04-30 15:30:37 +0900

    Modify Card Classes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { Typography, AppBar } from '@material-ui/core';
 import MusicList from './MusicList';
 import music_list from './server/data';
 
 function App() {
+    const [musiclist, setMusiclist] = useState(music_list);
 
     return (
         <div>
@@ -13,7 +14,7 @@ function App() {
                 </Typography>
             </AppBar>
             <div style={{height: 60, width: '100%'}}></div>
-            <MusicList list={music_list}></MusicList>
+            <MusicList list={musiclist}></MusicList>
         </div>
     );
 }
