0441515f61b4c2fd856712387305c4b3ff47cf0d
류 상원 <ryuuuuuuuu@ajou.ac.kr>
2022-04-30 15:30:37 +0900

    Modify Card Class

--- src/MusicList.js
+++ src/MusicList.js
@@ -10,8 +10,8 @@ const styles = {
         minWidth: 120,
         maxWidth: 300,
         marginBottom: 20,
-        // marginLeft: 'auto',
-        // marginRight: 'auto',
+        marginLeft: 'auto',
+        marginRight: 'auto',
     },
 };
 
@@ -25,7 +25,7 @@ function MusicList (props) {
         if (likes[id] === undefined){
             likes[id] = true;
         } else{
-            likes[id] = (likes[id]) ? false : true;
+            likes[id] = (likes[id]) ? false : true; 
         }
         setLikes({open: true, msg: `Thanks for response`});
     }
@@ -36,19 +36,20 @@ function MusicList (props) {
         }
         setSnackbar({open: false, msg: 'Unlike'});
     }
-    const {classes} = {props};
+    const classes = {props};
 
     return (
-        <div className={classes}>
+        <div className={props.classes}>
+            
             {props.list.results.map(item => {
                 return(
-                    <Card key={item.collectionId} variant="outlined">
-                        {/* <CardMedia 
+                    <Card key={item.collectionId} className={props.classes['card']}>
+                        <CardMedia 
                             component="img"
                             // height="100%"
                             image={item.artworkUrl100}
                             alt="Black Pink"
-                        /> */}
+                        />
                         <CardContent>
                             <Typography variant="subtitle1"> {item.artistName}</Typography>
                             <Typography variant="subtitle2"> {item.collectionCensoredName}</Typography>
